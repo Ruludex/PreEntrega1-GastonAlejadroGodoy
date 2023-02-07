f0519927874e795d2734996f8c6ffbc230ae552b
Ruludex <gastonghj@hotmail.com.ar>
2023-02-07 00:41:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import { Store } from './pages/Store';
+import { Index } from './pages/Index';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Index/>}/>
+        <Route path='Inicio' element = {<Index/>}/>
+        <Route path='Tienda' element = {<Store title="Bienvenido a mi tienda, sitio en construccion"/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
